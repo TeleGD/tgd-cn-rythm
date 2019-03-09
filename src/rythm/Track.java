4a68d70db21c94bfb1630a81254f1f36a566a24b
Océane Chazé <oceane.chaze@telecomnancy.eu>
2019-03-09 19:32:33 +0100

    Ajout de la classe Track.java.

---
+++ src/rythm/Track.java
@@ -0,0 +1,27 @@
+package rythm;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Track {
+	private int score;
+	private String date;
+	private String filename;
+	
+	public void update(GameContainer container, StateBasedGame game, int delta) {
+		
+	}
+	public void render(GameContainer container, StateBasedGame game, Graphics context) {
+		
+	}
+	public int lwbd(){
+		return 0;
+		
+	}
+	
+	public int getScore() {
+		return score;
+	}
+
+}
